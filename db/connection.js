9b45a7aa38a534a91918e9c5349d1728af3984c0
Nilton Dev <ngonzano.develop@gmail.com>
2025-05-23 18:40:00 -0500

    init server

--- db/connection.js
+++ db/connection.js
@@ -26,24 +26,22 @@ types.setTypeParser(1114, function (stringValue) {
 // Configuración de la base de datos
 const databaseConfig = {
   // PRODUCCION
-  // host: process.env.host,
-  // port: process.env.port,
-  // database: process.env.database,
-  // user: process.env.user,
-  // password: process.env.password,
-  // // ssl: {
-  // //     rejectUnauthorized: false
-  // // },
+  host: process.env.host,
+  port: process.env.port,
+  database: process.env.database,
+  user: process.env.user,
+  password: process.env.password,
+  ssl: {
+    rejectUnauthorized: false
+  },
 
   //QA
-  'host': 'localhost',  // Configuración de la base de datos local
-  'port': 5432,
-  'database': 'postgres',
-  'user': 'ngonzano',
-  'password': '14@qweszxC',  // Contraseña de la base de datos
-  //  ssl: {
-  //     rejectUnauthorized: false
-  // },
+  // 'host': 'localhost',
+  // 'port': 5432,
+  // 'database': 'postgres',
+  // 'user': 'ngonzano',
+  // 'password': '14@qweszxC',
+
 };
 
 // Crear la instancia de la base de datos usando pg-promise

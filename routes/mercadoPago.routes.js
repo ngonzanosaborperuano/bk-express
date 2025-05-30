3ef158c8197bf295947413e2c7ed3b85da9a3e0d
Nilton Dev <ngonzano.develop@gmail.com>
2025-05-30 12:53:27 -0500

    .

--- routes/mercadoPago.routes.js
+++ routes/mercadoPago.routes.js
@@ -3,7 +3,7 @@ import * as mercadoPagoController from '../controllers/mercadopago.controller.js
 const mercadoPagoRoutes = (app, upload) => {
   app.post('/crear-preferencia', mercadoPagoController.crearPreferenciaController);
   app.post('/notificacion', mercadoPagoController.sendWebHooks);
-  app.post('api/mercadoPago/webhook', mercadoPagoController.responseWebHooks);
+  app.post('/api/mercadoPago/webhook', mercadoPagoController.responseWebHooks);
 };
 
 export default mercadoPagoRoutes;

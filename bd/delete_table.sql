1911ea944e22116618808676164c5715b69976b8
ngonzanosaborperuano <saborperuanoapp@gmail.com>
2025-12-14 13:51:58 -0500

    .

---
+++ bd/delete_table.sql
@@ -0,0 +1,19 @@
+-- 🔴 Eliminar tablas si existen
+DROP TABLE IF EXISTS alternative_items,
+alternatives,
+ingredients,
+allergens,
+diets,
+instructions,
+plating_instructions,
+macros,
+nutrition_info,
+recommended_servings,
+medical_restrictions,
+health_warnings,
+extra_info,
+similar_dishes,
+recommended_pairings,
+recipes CASCADE;
+
+-- 🟢 Luego pegás aquí todo tu bloque CREATE TABLE
